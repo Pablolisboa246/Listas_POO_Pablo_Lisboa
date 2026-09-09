c0598076e1b5f2d5d5c66ca48f603946e44cde16
Pablo Lisboa <pablomatheus246@gmail.com>
2026-09-08 21:44:14 -0300

    Fix typo in input prompt for travel time

--- lista_01/lista01_questao_02.py
+++ lista_01/lista01_questao_02.py
@@ -1,4 +1,4 @@
-tempo_em_horas= float(input("digite o tempo de deslocameto em horas"))
+tempo_em_horas= float(input("digite o tempo de deslocamento em horas"))
 velocidade= 30*1000
 tempo_segundos= tempo_em_horas * 3600
 distancia= velocidade * tempo_segundos
